d75724f95235a633bd16b8cdf312a65f78f34f03
GregoryVDev <arexo@hotmail.fr>
2024-04-08 16:57:57 +0200

    Responsive 1024px miss just the "container-maki"

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 // NAVBAR BURGER
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
-
+const nav = document.getElementById("nav");
 const menuBurger = document.getElementById("burger");
 
 menuBurger.addEventListener("click", () => {
-  alert();
+  nav.classList.toggle("active");
+  menuBurger.classList.toggle("active");
 });
 
 // SOUND BIKE
